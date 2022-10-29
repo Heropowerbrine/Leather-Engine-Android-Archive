0148e6eff90e8bd019d19ea0a540a913f951e88f
Hero <78842166+Heropowerbrine@users.noreply.github.com>
2022-10-29 11:01:51 +0800

    read desc of this commit
    this is the function we need, but we need to change it for it to only read for directories in directories (e.g. a folder of a mod inside the mods folder)

--- source/Paths.hx
+++ source/Paths.hx
@@ -147,21 +147,21 @@ class Paths
 		return name.replace(' ', '-').toLowerCase();
 	
 	function recursiveLoop(directory:String = "path/to/") {
-  if (sys.FileSystem.exists(directory)) {
-    trace("directory found: " + directory);
-    for (file in sys.FileSystem.readDirectory(directory)) {
-      var path = haxe.io.Path.join([directory, file]);
-      if (!sys.FileSystem.isDirectory(path)) {
-        trace("file found: " + path);
-        // do something with file
-      } else {
-        var directory = haxe.io.Path.addTrailingSlash(path);
-        trace("directory found: " + directory);
-        recursiveLoop(directory);
-      }
-    }
-  } else {
-    trace('"$directory" does not exists');
-  }
-}
+          if (sys.FileSystem.exists(directory)) {
+            trace("directory found: " + directory);
+            for (file in sys.FileSystem.readDirectory(directory)) {
+              var path = haxe.io.Path.join([directory, file]);
+              if (!sys.FileSystem.isDirectory(path)) {
+                trace("file found: " + path);
+                // do something with file
+              } else {
+                var directory = haxe.io.Path.addTrailingSlash(path);
+                trace("directory found: " + directory);
+                recursiveLoop(directory);
+              }
+            }
+          } else {
+            trace('"$directory" does not exists');
+          }
+        }
 }
