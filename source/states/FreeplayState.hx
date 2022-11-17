4bb0c7768c729cf0644b558fcada2aaacb034731
Hero <78842166+Heropowerbrine@users.noreply.github.com>
2022-11-17 13:40:55 +0800

    removing that for a sec

--- source/states/FreeplayState.hx
+++ source/states/FreeplayState.hx
@@ -97,10 +97,8 @@ class FreeplayState extends MusicBeatState
 
 		if(FlxG.sound.music == null || !FlxG.sound.music.playing)
 			TitleState.playTitleMusic();
-		
-		var moddirectory = Paths.recursiveLoop(SUtil.getPath() + "mods/");
                 
-		var initSonglist = CoolUtil.coolTextFile(Paths.mods(moddirectory(Paths.appendmods(Paths.modtxt('freeplaySonglist')))));
+		var initSonglist = CoolUtil.coolTextFile(Paths.mods(Paths.appendmods(Paths.modtxt('freeplaySonglist'))));
 
 		#if discord_rpc
 		// Updating Discord Rich Presence
