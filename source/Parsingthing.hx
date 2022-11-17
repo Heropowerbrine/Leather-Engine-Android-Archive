7efa199c000a42920729d40eb9c9bff716702abb
Hero <78842166+Heropowerbrine@users.noreply.github.com>
2022-11-17 15:39:53 +0800

    hmm

--- source/Parsingthing.hx
+++ source/Parsingthing.hx
@@ -1,4 +1,4 @@
-package parse;
+package;
 
 import openfl.utils.Assets;
 import haxe.Json;
@@ -32,12 +32,12 @@ class Week
 		currentLoadedWeeks.clear();
 		weeksList = [];
 
-		final list:Array<String> = CoolUtil.coolTextFile(Paths.txt('weeks/weekList'));
+		final list:Array<String> = CoolUtil.coolTextFile(Paths.txt('_append/data/freeplaySonglist'));
 		for (i in 0...list.length)
 		{
 			if (!currentLoadedWeeks.exists(list[i]))
 			{
-				var week:SwagWeek = parseJson(Paths.json('weeks/' + list[i]));
+				var week:SwagWeek = parseJson(Paths.json('data/week data/' + list[i]));
 				if (week != null)
 				{
 					if (week != null && (isStoryMode && !week.hideStoryMode) || (!isStoryMode && !week.hideFreeplay))
