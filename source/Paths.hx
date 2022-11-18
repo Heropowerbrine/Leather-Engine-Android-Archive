796ae43cd7d6fe84ce23883a8353b9c2f846aa2c
Hero <78842166+Heropowerbrine@users.noreply.github.com>
2022-11-18 12:53:02 +0800

    bruhhhhhh

--- source/Paths.hx
+++ source/Paths.hx
@@ -160,7 +160,7 @@ class Paths
               } else {
                 var directory = haxe.io.Path.addTrailingSlash(path);
                 trace("directory found: " + directory);
-		return directory
+		return directory;
               }
             }
           } else {
