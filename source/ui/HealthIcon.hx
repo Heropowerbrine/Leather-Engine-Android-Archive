75ae0f56a4bd42bf3852c4e8709677624e5417cf
Hero <78842166+Heropowerbrine@users.noreply.github.com>
2022-11-19 12:36:24 +0800

    hoping this works
    (im trying to get it to fully read assets first)

--- source/ui/HealthIcon.hx
+++ source/ui/HealthIcon.hx
@@ -34,24 +34,24 @@ class HealthIcon extends TrackerSprite
 		antialiasing = true;
 
 		if(
-			Assets.exists(Paths.image('icons/' + char + '-icons').split(".png")[0] + ".xml") ||
-			Assets.exists(Paths.image('icons/icon-' + char).split(".png")[0] + ".xml") ||
-			Assets.exists(Paths.image('icons/' + char).split(".png")[0] + ".xml")
+			Paths.image('icons/' + char + '-icons').split(".png")[0] + ".xml" ||
+			Paths.image('icons/icon-' + char).split(".png")[0] + ".xml" ||
+			Paths.image('icons/' + char).split(".png")[0] + ".xml"
 		)
 		{
 			var selected = "your";
 
-			if(Assets.exists(Paths.image('icons/' + char + '-icons').split(".png")[0] + ".xml"))
+			if Paths.image('icons/' + char + '-icons').split(".png")[0] + ".xml")
 			{
 				frames = Paths.getSparrowAtlas('icons/' + char + '-icons');
 				selected = Paths.image('icons/' + char + '-icons');
 			}
-			else if(Assets.exists(Paths.image('icons/icon-' + char).split(".png")[0] + ".xml"))
+			else if(Paths.image('icons/icon-' + char).split(".png")[0] + ".xml")
 			{
 				frames = Paths.getSparrowAtlas('icons/icon-' + char);
 				selected = Paths.image('icons/icon-' + char);
 			}
-			else if(Assets.exists(Paths.image('icons/' + char).split(".png")[0] + ".xml"))
+			else if(Paths.image('icons/' + char).split(".png")[0] + ".xml")
 			{
 				frames = Paths.getSparrowAtlas('icons/' + char);
 				selected = Paths.image('icons/' + char);
@@ -59,7 +59,7 @@ class HealthIcon extends TrackerSprite
 
 			animation.addByPrefix(char, char, 24, true, isPlayer);
 
-			if(Assets.exists(selected.split(".png")[0] + ".txt"))
+			if(selected.split(".png")[0] + ".txt")
 			{
 				var theFunny = Assets.getText(selected.split(".png")[0] + ".txt").split(" ");
 
@@ -74,11 +74,11 @@ class HealthIcon extends TrackerSprite
 		}
 		else
 		{
-			if(Assets.exists(Paths.image('icons/' + char + '-icons'))) // LE ICONS
+			if(Paths.image('icons/' + char + '-icons')) // LE ICONS
 				loadGraphic(Paths.image('icons/' + char + '-icons'), true, 150, 150);
-			else if(Assets.exists(Paths.image('icons/' + 'icon-' + char))) // PSYCH ICONS
+			else if(Paths.image('icons/' + 'icon-' + char)) // PSYCH ICONS
 				loadGraphic(Paths.image('icons/' + 'icon-' + char), true, 150, 150);
-			else if(Assets.exists(Paths.image('icons/' + char))) // lmao image file names i guess if you're really lazy
+			else if(Paths.image('icons/' + char)) // lmao image file names i guess if you're really lazy
 				loadGraphic(Paths.image('icons/' + char), true, 150, 150);
 			else // UNKNOWN ICON
 				loadGraphic(Paths.image('icons/placeholder-icon'), true, 150, 150);
