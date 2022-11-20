971fd5bb4b849e232a280e818dbd7e06602fd8f4
Hero <78842166+Heropowerbrine@users.noreply.github.com>
2022-11-20 16:33:08 +0800

    Update JsonReader.hx

--- source/JsonReader.hx
+++ source/JsonReader.hx
@@ -1,4 +1,4 @@
-package spine.support.utils;
+package;
 
 import spine.support.files.FileHandle;
 import spine.support.utils.JsonValue;
@@ -8,13 +8,13 @@ class JsonReader {
 
     public function new() {}
 
-    public function parse(file:FileHandle):JsonValue {
+    inline public function parse(file:FileHandle):JsonValue {
 
         return new JsonDynamic(Json.parse(file.getContent()));
 
     }
 
-    public static function parseString(str:String):JsonValue {
+    inline public static function parseString(str:String):JsonValue {
 
         return new JsonDynamic(Json.parse(str));
 
