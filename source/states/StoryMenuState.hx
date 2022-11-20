59390565b000e25dd14afa50db4c512d038881c4
Hero <78842166+Heropowerbrine@users.noreply.github.com>
2022-11-20 18:40:18 +0800

    Update StoryMenuState.hx

--- source/states/StoryMenuState.hx
+++ source/states/StoryMenuState.hx
@@ -475,7 +475,7 @@ class StoryMenuState extends MusicBeatState
 
 	function loadJSON(name:String)
 	{
-		var group:StoryGroup = cast JsonReader.parseString(Paths.json("week data/" + name));
+		var group:StoryGroup = cast JsonReader.parseString(Assets.getText(Paths.json("week data/" + name)));
 
 		for(week in group.weeks)
 		{
