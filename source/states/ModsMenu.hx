66c88b97287b6a7b78ad004a78cb5be00f5aa1e7
Hero <78842166+Heropowerbrine@users.noreply.github.com>
2022-11-28 11:12:12 +0800

    mods menu works fine, its not it

--- source/states/ModsMenu.hx
+++ source/states/ModsMenu.hx
@@ -75,7 +75,7 @@ class ModsMenu extends MusicBeatState
 		if(FlxG.sound.music == null)
 			FlxG.sound.playMusic(MusicUtilities.GetOptionsMenuMusic(), 0.7, true);
 
-		SUtil.getPath() + PolymodHandler.loadModMetadata();
+	        PolymodHandler.loadModMetadata();
 
 		loadMods();
 
